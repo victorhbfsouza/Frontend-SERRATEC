97a70727e13dfd1b44e0bff7b5f7e15cb8b39c0d
Victor Hugo Souza <127329074+victorhbfsouza@users.noreply.github.com>
2023-05-05 21:43:15 -0300

    Atualização 05/05/2023

--- Trabalho Frontend/Minha parte/js/cadastro.js
+++ Trabalho Frontend/Minha parte/js/cadastro.js
@@ -1,38 +1,23 @@
-var usuario = [
-    {
-        "email": "12345678@gmail.com",
-        "senha": "MTIzNDU2Nzg=",
-        "confirmacaoSenha": "MTIzNDU2Nzg=",
-        "endereco": "Rua A n. 1"
-    },
-    {
-        "email": "G4121212@gmail.com",
-        "senha": "RzQxMjEyMTI=",
-        "confirmacaoSenha": "RzQxMjEyMTI=",
-        "endereco": "Rua B n. 2"
-    }
-]
-
 const getEmail = document.getElementById("email");
 const getSenha = document.getElementById("senha");
-const getnewConfirmacaoSenha = document.getElementById("confirmacaoSenha");
-const getnewEndereco = document.getElementById("endereco");
+const getConfirmacaoSenha = document.getElementById("confirmacaoSenha");
+const getEndereco = document.getElementById("endereco");
 
 function cadastrar() {
     var confirmacao = localStorage.getItem(getEmail.value);
 
     if (confirmacao != null) {
-        alert("Usuário já existente")
-    }
-
-    else if (confirmacao === null && getSenha.value === getnewConfirmacaoSenha.value) {
-        localStorage.setItem(getEmail.value, window.btoa(getnewConfirmacaoSenha.value))
+        alert("Usuário já existente");
+    } 
+    else if (getSenha.value !== getConfirmacaoSenha.value) {
+        alert("As senhas não conferem, verifique e tente novamente!");
+    } 
+    else {
+        localStorage.setItem(getEmail.value, window.btoa(getSenha.value));
+        localStorage.setItem(getEmail.value + '_endereco', getEndereco.value);
         alert("Usuário cadastrado com sucesso!");
     }
-    else {
-            alert("As senhas não conferem, verifique e tente novamente!")
-        }
-    }
+}
 
     function olharSenha() {
         var typePassword = document.getElementById("senha");
